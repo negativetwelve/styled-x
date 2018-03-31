b8d6e3ff86c06efdd1dad5f31ad9bde693fddd25
Mark Miyashita <negativetwelve@gmail.com>
2018-03-31 01:34:11 -0700

    Create the styled-x-text-input component

---
+++ packages/styled-x-text-input/src/TextInput.js
@@ -0,0 +1,8 @@
+// Libraries
+import Styled from 'styled-x-primitives';
+import ReactXTextInput from 'react-x-text-input';
+
+const TextInput = Styled(ReactXTextInput)`
+`;
+
+export default TextInput;
