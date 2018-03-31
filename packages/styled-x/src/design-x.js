2a18db1e64538dbd42d5c74de7b6a2b4278cd3ae
Mark Miyashita <negativetwelve@gmail.com>
2018-03-31 00:37:55 -0700

    Use styled-x-primitives to construct the Row and Button components

--- packages/styled-x/src/design-x.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable brace-style */
-module.exports = {
-  get Button() { return require('styled-x-button').default; },
-  get Row() { return require('styled-x-row').default; },
-};
---
+++ packages/styled-x/src/styled-x.js
@@ -0,0 +1,9 @@
+// Components
+import Styled from 'styled-x-primitives';
+import Button from 'styled-x-button';
+import Row from 'styled-x-row';
+
+Styled.Button = Button;
+Styled.Row = Row;
+
+export default Styled;
