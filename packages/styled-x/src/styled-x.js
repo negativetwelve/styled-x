056437928bd69a89ead4a15402ec9c726d93753e
Mark Miyashita <negativetwelve@gmail.com>
2018-03-31 01:39:00 -0700

    Add the styled-x-text-input to the styled-x main export

--- packages/styled-x/src/styled-x.js
+++ packages/styled-x/src/styled-x.js
@@ -2,8 +2,10 @@
 import Styled from 'styled-x-primitives';
 import Button from 'styled-x-button';
 import Row from 'styled-x-row';
+import TextInput from 'styled-x-text-input';
 
 Styled.Button = Button;
 Styled.Row = Row;
+Styled.TextInput = TextInput;
 
 export default Styled;
