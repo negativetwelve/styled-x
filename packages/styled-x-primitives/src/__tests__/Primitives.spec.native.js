da4a460a306351be1ed6c33baffcb32a71c0a9c3
Mark Miyashita <negativetwelve@gmail.com>
2018-03-30 23:51:58 -0700

    [styled-x-primitives] Create the styled-x-primitives package + add test infrastructure (#2)
    * Create the styled-x-primitives package
    
    * Add test infrastructure
    
    * Add test snapshots for the styled-x-primitives package

---
+++ packages/styled-x-primitives/src/__tests__/Primitives.spec.native.js
@@ -0,0 +1,15 @@
+// Modules
+import React from 'react';
+import Primitives from '../Primitives';
+
+const Title = Primitives.Text`
+  font-size: 16px;
+  text-align: center;
+`;
+
+/* eslint-disable no-undef */
+describe('Primitives`', () => {
+  describe('Title', () => {
+    itRenders(() => <Title />);
+  });
+});
