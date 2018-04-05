80a63ceb1ee2f9f9186d8d9729e9539c811f6894
Mark Miyashita <negativetwelve@gmail.com>
2018-04-05 00:38:06 -0700

    Update packages to depend on react-x-primitives to include dependencies for react, react-native, react-native-web

--- packages/styled-x-primitives/src/Primitives.js
+++ packages/styled-x-primitives/src/Primitives.js
@@ -1,12 +1,14 @@
 // Libraries
-import Primitives from 'react-primitives';
+import {Image, Text, Touchable, View} from 'react-x-primitives';
 import createComponent from 'styled-x-create-component';
 
 // Inject Primitives to Styled that we can use `.extend` on.
-Primitives.Image = createComponent(Primitives.Image);
-Primitives.Text = createComponent(Primitives.Text);
-Primitives.Touchable = createComponent(Primitives.Touchable);
-Primitives.View = createComponent(Primitives.View);
+const Primitives = {
+  Image: createComponent(Image),
+  Text: createComponent(Text),
+  Touchable: createComponent(Touchable),
+  View: createComponent(View),
+};
 
 // This allows us to import the entire Primitives object or specific
 // components via `import {View} from 'styled-x-primitives';`.
