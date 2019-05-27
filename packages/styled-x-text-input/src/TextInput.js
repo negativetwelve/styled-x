0f66a5e1cbc059ddb09e20c0f7b050b0ca5f0e2b
Mark Miyashita <negativetwelve@gmail.com>
2019-05-26 19:17:29 -0700

    Remove the styled-x-row, styled-x-button, and styled-x-text-input components (#25)

--- packages/styled-x-text-input/src/TextInput.js
+++
@@ -1,5 +0,0 @@
-// Libraries
-import createComponent from 'styled-x-create-component';
-import TextInput from 'react-x-text-input';
-
-export default createComponent(TextInput);
--- packages/styled-x/src/styled-x.js
+++ packages/styled-x/src/styled-x.js
@@ -2,21 +2,15 @@
 import styled from 'styled-components/primitives';
 
 // Components
-import Button from 'styled-x-button';
 import {Image, Text, Touchable, View} from 'styled-x-primitives';
-import Row from 'styled-x-row';
-import TextInput from 'styled-x-text-input';
 
 const Styled = (tag) => styled(tag);
 
-Styled.Button = Button;
 Styled.Image = Image;
-Styled.Row = Row;
 Styled.Text = Text;
-Styled.TextInput = TextInput;
 Styled.Touchable = Touchable;
 Styled.View = View;
 
 // This allows us to import either the entire Styled object or individual
-// imports such as `import {Button} from 'styled-x';`.
+// imports such as `import {Image, Text, View} from 'styled-x';`.
 module.exports = Styled;
